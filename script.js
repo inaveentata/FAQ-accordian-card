bf0468f8d92fa2c5de4b7f8bf7f7221bc0eb78f4
inaveentata <inaveentata@outlook.com>
2022-05-27 01:41:30 +0530

    Changing funcionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const icons = document.querySelectorAll(".icon");
 
 icons.forEach((icon) => {
@@ -10,3 +11,6 @@ icons.forEach((icon) => {
     targetElement.classList.toggle("show-height");
   });
 });
+
+
+
