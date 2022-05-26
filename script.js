752449d3d476c12a2ffbb0a12a160fdc498d19e8
inaveentata <inaveentata@outlook.com>
2022-05-27 01:30:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const icons = document.querySelectorAll(".icon");
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    const element = e.target.parentElement;
+    const sibiling = element.previousElementSibling;
+    const parent = element.parentElement;
+    const targetElement = parent.nextElementSibling;
+    sibiling.classList.toggle("font-weight");
+    targetElement.classList.toggle("show-height");
+  });
+});
